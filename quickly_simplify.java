c4d68418493024a3cdc0162ae88f5981afb744d6
guoAsdwen <18610895587@163.com>
2026-07-08 15:20:24 +0800

    Add files via upload
    一个快速的二次根式化简，输入数字<10^18,不使用素数打表

--- quickly_simplify.java
+++ quickly_simplify.java
@@ -2,14 +2,14 @@
 
 public class quickly_simplify {
     public static String quickly_simplify(long original) {
-    int d = 0;
+    long d = 0;
+    long remain = original;
     double original_sqrt = Math.sqrt(original);
     double max_root = Math.sqrt(original>>1);
-    int original_sqrt_int = (int) original_sqrt;
     int out = 1;
+    int original_sqrt_int = (int) original_sqrt;
     int odd_max_root = (int)max_root>>1;
     odd_max_root = (odd_max_root<<1)+1;
-    long remain = original;
     if((original_sqrt-original_sqrt_int)==0) {
 	    return(String.valueOf(original_sqrt_int));
     }else {
@@ -17,7 +17,7 @@ public class quickly_simplify {
 		if (remain%(d*d)== 0) {
 			remain = remain/(d*d);
 			odd_max_root = (int)Math.sqrt(remain>>1);
-			out = out*d;
+			out = out*(int)d;
 			d = 0;
 		}
 	}
@@ -25,7 +25,7 @@ public class quickly_simplify {
 		if (remain%(d*d)== 0) {
 			remain = remain/(d*d);
 			odd_max_root = (int)Math.sqrt(remain>>1);
-			out = out*d;
+			out = out*(int)d;
 			d = 1;
 		}
 	}
